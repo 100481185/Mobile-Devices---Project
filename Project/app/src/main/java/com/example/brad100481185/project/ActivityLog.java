d276a1b8c71a1451662874ff7fc3d7573c053bb0
unknown <100481185@UOSL15-R902678P.oncampus.local>
2015-11-28 17:16:03 -0500

    Spinner functionality added

--- Project/app/src/main/java/com/example/brad100481185/project/ActivityLog.java
+++ Project/app/src/main/java/com/example/brad100481185/project/ActivityLog.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ActivityLog extends Activity {
 
@@ -11,6 +14,26 @@ public class ActivityLog extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_log);
+
+        Spinner events = (Spinner)findViewById(R.id.spinner);
+        /*
+        ArrayAdapter<String> adapt = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_drpodown, array);
+        events.setAdapter(adapt);
+        events.setSelection(0);
+         */
+
+        events.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView chosen = (TextView)findViewById(R.id.eventName);
+                //chosen.setText();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     //todo: proceed to review activity
